0198f8c239f89b0964137e43db175b8d99ab20cb
Ivin <ivinrodriguees@gmail.com>
2024-05-23 19:35:45 -0300

    feat(src): new files to help explain in presentation

---
+++ src/scripts/index.js
@@ -0,0 +1,9 @@
+import { carousel } from "carousel-lib";
+
+function carouselInit() {
+  const carouselElement = document.querySelector(".carousel");
+
+  carousel(carouselElement);
+}
+
+carouselInit();
