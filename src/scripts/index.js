544421088f6915c125e71fc83b87321ad56561f2
Ivin Rodrigues <51685888+IvinRodrigues@users.noreply.github.com>
2024-06-06 20:13:43 -0300

    Delete src directory

--- src/scripts/index.js
+++
@@ -1,9 +0,0 @@
-import { carousel } from "carousel-lib";
-
-function carouselInit() {
-  const carouselElement = document.querySelector(".carousel");
-
-  carousel(carouselElement);
-}
-
-carouselInit();
